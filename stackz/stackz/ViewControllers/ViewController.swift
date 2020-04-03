c7da091292f45146b1c796bbbbca06f1724c3844
James <jameswsj@berkeley.edu>
2020-04-02 17:14:35 -0700

    installing dependencies

---
+++ stackz/stackz/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stackz
+//
+//  Created by James Jung on 4/2/20.
+//  Copyright © 2020 James Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
