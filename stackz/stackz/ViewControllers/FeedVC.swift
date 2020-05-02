b24c33b0180991e1b082c57734d631e3acffb93f
James <jameswsj@berkeley.edu>
2020-05-02 05:30:48 -0700

    finished feed

--- stackz/stackz/ViewControllers/FeedVC.swift
+++ stackz/stackz/ViewControllers/FeedVC.swift
@@ -8,27 +8,51 @@
 import UIKit
 import Foundation
 import Koloda
+import Firebase
 
 class FeedVC: UIViewController {
     @IBOutlet weak var cardView: KolodaView!
     var post_stacks: [postView] = []
     var post_array: [Post] = []
+    var ref = Database.database().reference()
     
     @IBAction func savePost(_ sender: Any) {
         Firebase.addCurUserSaved(post: post_array[cardView.currentCardIndex])
         util.displayAlert(title: "Post Saved!", message: "Check your Saved to view", vc: self)
     }
     override func viewDidLoad() {
-        super.viewDidLoad()
+        post_array = accessPostsInDB()
         cardView.delegate = self
         cardView.dataSource = self
-        for feed in demoFeed {
-            post_stacks.append(postView(frame: CGRect(x: 0, y: 0, width: 300, height: 500), title: feed.title, text: feed.text))
+//        for feed in post_array {
+//            post_stacks.append(postView(frame: CGRect(x: 0, y: 0, width: 300, height: 500), title: feed.title, text: feed.text))
+//        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            super.viewDidLoad()
+            print(self.post_array)
+            print(self.post_stacks)
+            self.cardView.reloadData()
         }
-        print(post_stacks)
-        cardView.reloadData()
-        
     }
     
+    func accessPostsInDB() -> [Post] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yy"
+        let postRef = ref.child("posts")
+        var allPosts: [Post] = []
+        let refHandle = postRef.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            
+            for (key, val) in postDict {
+                print(key, val)
+                let post = Post(title: val["title"] as! String, text: val["text"] as! String)
+                post.date = formatter.date(from: val["date"] as! String)!
+                self.post_array.append(post)
+                self.post_stacks.append(postView(frame: CGRect(x: 0, y: 0, width: 300, height: 500), title: post.title, text: post.text))
+            }
+        })
+        self.cardView.reloadData()
+        return allPosts
+    }
 
 }
