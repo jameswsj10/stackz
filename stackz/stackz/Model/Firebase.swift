a79738b8d4c4cfdd46916846e35f47340ea58b0b
theresac23 <theresachen@berkeley.edu>
2020-05-02 02:08:19 -0700

    added user db

--- stackz/stackz/Model/Firebase.swift
+++ stackz/stackz/Model/Firebase.swift
@@ -17,6 +17,7 @@ class Firebase {
     static var ref = Database.database().reference()
 
     static func addPostToDB(post: Post) {
+        var user = Auth.auth().currentUser;
         let formatter = DateFormatter()
         formatter.dateStyle = .short
         let postID = self.ref.child("posts").childByAutoId().key
@@ -25,8 +26,51 @@ class Firebase {
                     "text" : post.text!,
                     "date" : formatter.string(from: post.date),
                     "comments" : post.comments])
-        
+        // add post to current user's my post children
+        self.ref.child("Users/\(user!.uid)/myPosts").updateChildValues([
+            "title" : post.title!,
+            "text" : post.text!,
+            "date" : formatter.string(from: post.date),
+            "comments" : post.comments])
+            
     }
+    
+    static func accessCurUserPosts() -> [Post] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        var user = Auth.auth().currentUser;
+        let postRef = ref.child("Users/\(user!.uid)/myPosts")
+        var curUserPosts: [Post] = []
+        let refHandle = postRef.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            
+            for (key, val) in postDict {
+                let post = Post(title: val["title"] as! String, text: val["text"] as! String)
+                post.date = formatter.date(from: val["date"] as! String)!
+                curUserPosts.append(post)
+            }
+        })
+        return curUserPosts
+    }
+    
+    static func accessCurUserSaved() -> [Post] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        var user = Auth.auth().currentUser;
+        let postRef = ref.child("Users/\(user!.uid)/saved")
+        var curUserPosts: [Post] = []
+        let refHandle = postRef.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            
+            for (key, val) in postDict {
+                let post = Post(title: val["title"] as! String, text: val["text"] as! String)
+                post.date = formatter.date(from: val["date"] as! String)!
+                curUserPosts.append(post)
+            }
+        })
+        return curUserPosts
+    }
+
     
     static func accessPostsInDB() -> [Post] {
         let formatter = DateFormatter()
